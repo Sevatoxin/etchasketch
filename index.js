1b0add0c3945baf4241843f51eca6b3e41c3aad5
Seva <137809136+Sevatoxin@users.noreply.github.com>
2024-05-19 19:49:27 +0200

    Added commented description on what to do

---
+++ index.js
@@ -0,0 +1,7 @@
+// Creating a EtchASketch Website to draw on a grid of pixels.
+// User should be able to:
+// - Change divs on grid background color with a mouse hover
+// - To change background color one can: change class or change background color on Event
+// - Change grid size
+// - Reset the grid
+
