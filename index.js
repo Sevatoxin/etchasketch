e3a1cfe4b14394a01ce67f0b09489bd512fc91ee
Seva <137809136+Sevatoxin@users.noreply.github.com>
2024-05-20 10:01:47 +0200

    Cells of grid can be painted and grid can cleared

--- index.js
+++ index.js
@@ -7,18 +7,42 @@
 
 //Grab elements from HTML document
 const gridContainer = document.querySelector(".grid.container");
+const createButton = document.querySelector(".grid-button");
+const deleteGridButton = document.querySelector(".grid-clear");
+
+
+//Setting up button functions
+createButton.addEventListener("click", () => addGrid(16));
+deleteGridButton.addEventListener("click", () => {
+    boxes.forEach((box) => {
+        box.setAttribute("style", "background-color: blanchedalmond;");
+        // box.remove();
+    })});
+
+//Grid draw function for hover event
+function paintCell(e) {
+    const cell = e.target;
+    cell.setAttribute("style", "background-color: red;" +
+        "transition: background-color 0.2s;"
+    );
+}
 
 //Setting up the grid
 let boxes = [];
-addGrid(16);
 
 function addGrid (size) {
+    //Delete grid before creating a new one
+    if (boxes) {
+        boxes.forEach((box) => {
+            box.remove();
+        })
+    }
+    //Create the grid with size of slider
     for (let i = 0; i < (size * size); i++) {
         const box = document.createElement("div");
         box.className = "box";
+        box.addEventListener("mouseover", paintCell);
         gridContainer.appendChild(box);
         boxes.push(box);
     }
 }
-
-console.log(boxes.length)
